ae5ea7667ecb7b6933f4c3d3826fcfe8e5654c8c
Tomas Ramos <tomas.ramosv21@gmail.com>
2020-06-26 16:07:35 +0800

    Fixed formatting

--- cpuid.s
+++ cpuid.s
@@ -24,10 +24,10 @@ _main:
     movq $0x0, %rax         # Define stdin as the output location for the cpuid call
     cpuid                   # Then call cpuid
     leaq output(%rip), %rsi # Get pointer to the string
-    movq %rbx, 0x1B(%rsi)     # Add information to the string x3
+    movq %rbx, 0x1B(%rsi)   # Add information to the string x3
     movq %rdx, 0x1F(%rsi)
     movq %rcx, 0x23(%rsi)
-    movq $0xA, 0x27(%rsi)     # Add new line character
+    movq $0xA, 0x27(%rsi)   # Add new line character
 
     movq $0x2000004, %rax   # System call number, remember the string in already pointed to by RSI
     movq $0x1, %rdi         # Pass the file descriptor
